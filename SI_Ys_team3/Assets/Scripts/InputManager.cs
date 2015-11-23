81ef144e0b18a25e7d3830c223f48c9f7f4e1533
Rodrigue Maurouard <r.maurouard@rubika-edu.com>
2015-11-23 19:59:07 +0100

    First commit

---
+++ SI_Ys_team3/Assets/Scripts/InputManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    public static InputManager instance = null;
+
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+	    
+        
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Clic gauche");
+        }
+
+        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Z");
+        }
+        else if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Q");
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("S");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("E");
+        }
+    }
+}
---
+++ SI_Ys_team3/Assets/Scripts/MapManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapManager : MonoBehaviour {
+
+    public static MapManager instance = null;
+
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        map = new GameObject[width, height];
+    }
+
+
+    public int width = 3;
+    public int height = 4 ;
+
+    public GameObject prefab;
+    public GameObject[,] map;
+
+
+	// Use this for initialization
+	void Start () {
+
+        float startPos = 0.2f;
+        float endPos = 0.8f;
+
+        for(int i = 0; i < width; i++)
+        {
+            for(int j =0; j < height; j++)
+            {
+                Vector3 position = Camera.main.ViewportToWorldPoint(new Vector3( ((float) i /width)*endPos+startPos, ((float) j /height) * endPos + startPos, 0.0f)) ;
+                position.z = 0;
+                map[i, j] =  Instantiate(prefab, position, Quaternion.identity) as GameObject;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
