f21ab1b8484763fe6c9824180f80507168f88be3
unknown <r.mathieu@J001-0140-15.etudiant.rubika-edu.com.local>
2015-11-24 16:08:05 +0100

    Text files and map / editing map done.

---
+++ SI_Ys_team3/Assets/Editor/WallsModifier.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class WallsModifier : EditorWindow {
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Walls/Modify Walls")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        WallsModifier window = (WallsModifier)EditorWindow.GetWindow(typeof(WallsModifier));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        if(Selection.gameObjects.Length == 1 && Selection.gameObjects[0].name == "Cell")
+        {
+            GameObject go = Selection.gameObjects[0];
+
+            GUILayout.Label("Change activated walls on selected cell", EditorStyles.boldLabel);
+            go.transform.FindChild("WallUp").gameObject.SetActive(EditorGUILayout.Toggle("Upper Wall",
+                                                                                         go.transform.FindChild("WallUp").gameObject.activeInHierarchy));
+            go.transform.FindChild("WallDown").gameObject.SetActive(EditorGUILayout.Toggle("Down Wall",
+                                                                                         go.transform.FindChild("WallDown").gameObject.activeInHierarchy));
+            go.transform.FindChild("WallLeft").gameObject.SetActive(EditorGUILayout.Toggle("Left Wall",
+                                                                                         go.transform.FindChild("WallLeft").gameObject.activeInHierarchy));
+            go.transform.FindChild("WallRight").gameObject.SetActive(EditorGUILayout.Toggle("Right Wall",
+                                                                                         go.transform.FindChild("WallRight").gameObject.activeInHierarchy));
+        }
+        else
+        {
+            GUILayout.Label("Select one cell to modify the walls", EditorStyles.boldLabel);
+        }
+        
+        
+    }
+}
--- SI_Ys_team3/Assets/Scripts/MapManager.cs
+++ SI_Ys_team3/Assets/Scripts/MapManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MapManager : MonoBehaviour {
 
@@ -9,7 +10,6 @@ public class MapManager : MonoBehaviour {
     {
         instance = this;
         DontDestroyOnLoad(gameObject);
-        map = new GameObject[width, height];
     }
 
 
@@ -17,22 +17,28 @@ public class MapManager : MonoBehaviour {
     public int height = 4 ;
 
     public GameObject prefab;
-    public GameObject[,] map;
+    public List<GameObject> map;
 
 
 	// Use this for initialization
 	void Start () {
 
-        float startPos = 0.2f;
-        float endPos = 0.8f;
+        //map = new GameObject[width, height];
+        map = new List<GameObject>();
+
+        /*float startPos = 0.2f;
+        float endPos = 0.8f;*/
+
+        GameObject go = GameObject.Find("Cells");
 
         for(int i = 0; i < width; i++)
         {
             for(int j =0; j < height; j++)
             {
-                Vector3 position = Camera.main.ViewportToWorldPoint(new Vector3( ((float) i /width)*endPos+startPos, ((float) j /height) * endPos + startPos, 0.0f)) ;
+                /*Vector3 position = Camera.main.ViewportToWorldPoint(new Vector3( ((float) i /width)*endPos+startPos, ((float) j /height) * endPos + startPos, 0.0f)) ;
                 position.z = 0;
-                map[i, j] =  Instantiate(prefab, position, Quaternion.identity) as GameObject;
+                map[i, j] =  Instantiate(prefab, position, Quaternion.identity) as GameObject;*/
+                map.Add(go.transform.GetChild(i * height + j).gameObject);
             }
         }
 	}
